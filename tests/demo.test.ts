24a0109b1b1c62ebcc3940e403a4d499ae8b51ea
Santi Racca <santiagoracca7@gmail.com>
2023-08-31 13:25:15 -0400

    init commit

---
+++ tests/demo.test.ts
@@ -0,0 +1,33 @@
+import { expect, use } from 'chai'
+import { sha256, toByteString } from 'scrypt-ts'
+import { Demo } from '../src/contracts/demo'
+import { getDefaultSigner } from './utils/txHelper'
+import chaiAsPromised from 'chai-as-promised'
+use(chaiAsPromised)
+
+describe('Test SmartContract `Demo`', () => {
+    let instance: Demo
+
+    before(async () => {
+        await Demo.compile()
+        instance = new Demo(sha256(toByteString('hello world', true)))
+        await instance.connect(getDefaultSigner())
+    })
+
+    it('should pass the public method unit test successfully.', async () => {
+        await instance.deploy(1)
+
+        const call = async () =>
+            instance.methods.unlock(toByteString('hello world', true))
+
+        await expect(call()).not.to.be.rejected
+    })
+
+    it('should throw with wrong message.', async () => {
+        await instance.deploy(1)
+
+        const call = async () =>
+            instance.methods.unlock(toByteString('wrong message', true))
+        await expect(call()).to.be.rejectedWith(/Hash does not match/)
+    })
+})
